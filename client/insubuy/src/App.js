30ba5fc6192003989ab192bb0ee288f612b052db
Aaron Estes <aestesc@gmail.com>
2019-04-25 11:54:05 -0500

    Results rendered in App component

--- client/insubuy/src/App.js
+++ client/insubuy/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import QuoteForm from './Components/QuoteForm/QuoteForm';
+import ResultsPage from './Components/ResultsPage/ResultsPage';
 
 function App() {
   return (
     <div className="App">
       <QuoteForm />
+      <ResultsPage />
     </div>
   );
 }
